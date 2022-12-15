4a5b0d84a7663710162873d63568d648960e95b9
Katie Pestotnik <katie.pestotnik@gmail.com>
2022-12-14 23:43:53 -0700

    add feedback

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+Nice job adding the component diagram! I would probably use a different shape for items that are not components as you have App, Button, and Display<br>
+getRandomGif and gif are props/state<br>
+
 **Giphy React App**
 ##Here I will render a page with no iamge and when the App sees the button click. The app will render a random Gif
 
--- src/App.js
+++ src/App.js
@@ -4,7 +4,18 @@ import Display from "./components/Display";
 
 import "./App.css";
 // const API = process.env.API;
+//this is how I used process.env
+//  const API_KEY = `${process.env.REACT_APP_GIPHY_API_KEY}`
 function App() {
+    //example of search api call
+  // const [tag, setTag] = useState(null)
+  // const getTag = async (input) => {
+  //   const response = await fetch(
+  //     `https://api.giphy.com/v1/stickers/random?api_key=${API_KEY}&tag=${input}`
+  //   )
+  //   const data = await response.json()
+  //   setTag(data)
+  // }
   const [gif, setRandomGif] = useState({});
 
   const getRandomGif = async () => {
